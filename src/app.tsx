bf31742789bda1066949e60847bd006b4b3586e1
marialluiza <malululicha@gmail.com>
2024-04-29 10:18:58 -0300

    implentação de contador

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { ChangeEvent, useState } from 'react'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
+import { Contador } from './components/contador';
 
 interface Note {
   id: string
@@ -16,14 +17,14 @@ export function App() {
   const [notes, setNotes] = useState<Note[]>(() => {
     const noteOnStorage = localStorage.getItem('notes')
 
-    if (noteOnStorage){
+    if (noteOnStorage) {
       return JSON.parse(noteOnStorage)
     }
 
     return []
   })
 
-  function onNoteCreated(content: string){
+  function onNoteCreated(content: string) {
     const newNote = {
       id: uuidv4(), //'crypto.randomUUID' gera id unico universal em formato de string
       date: new Date(),
@@ -37,7 +38,7 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
-  function onNoteDeleted(id: string){
+  function onNoteDeleted(id: string) {
     const notesArray = notes.filter(note => {
       return note.id != id
     })
@@ -47,43 +48,51 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
-  function handleSearch(event: ChangeEvent<HTMLInputElement>){
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
     const query = event.target.value
 
     setSearch(query)
   }
 
   const filteredNotes = search != ''
-    ? notes.filter(note => note.content.toLowerCase().includes(search.toLowerCase()) )  
+    ? notes.filter(note => note.content.toLowerCase().includes(search.toLowerCase()))
     : notes
 
   return (
     <div className="mx-auto max-w-6xl my-12 space-y-6 px-5" >
 
-      <img src={logo} className='w-40 h-auto'/>
+      <div className="flex justify-between">
+        <img src={logo} className='w-40 h-auto' />
 
-      <form className= "w-full mt-4">
-        <input 
-          type="text"   
+        <div className='flex mt-4'>
+          <Contador />
+        </div>
+
+      </div>
+
+      <form className="w-full mt-4">
+        <input
+          type="text"
           placeholder='Busque em suas notas...'
           className="w-full bg-transparent text-3xl font-normal tracking-tight outline-none placeholder:text-slate-500 "
           onChange={handleSearch}
         />
       </form>
 
-      <div className="h-px bg-slate-700"/>
+      <div className="h-px bg-slate-700" />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px] ">
-        
-        <NewNoteCard onNoteCreated={onNoteCreated}/>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px] ">
+
+        <NewNoteCard onNoteCreated={onNoteCreated} />
 
         {/*como as infos das notas estão dentro de um array, usa-se 'map' para percorrer o array */}
         {/* 'key' é uma propriedade do React, ele usa para deixar a aplicação mais performática e se basear nessa key pra saber "com quem" fazer algo*/}
         {filteredNotes.map(note => {
-          return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted}/>
+          return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />
         })}
 
       </div>
+
     </div>
 
   )
---
+++ src/components/contador.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Contador: React.FC = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <a href='https://contador.s12.com.br'>
+                <img src='https://contador.s12.com.br/img-x00cY3azb6Z7Awa5-59.gif' alt='counter free' />
+            </a>
+            <script type='text/javascript' src='https://contador.s12.com.br/ad.js?id=x00cY3azb6Z7Awa5'>
+            </script>
+        </div>
+    );
+};
